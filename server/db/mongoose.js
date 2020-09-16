d6c9fd3a6f7ece0fa811d332988f57895b887620
Rajat Kumar <kumar.rajatprajapati@gmail.com>
2020-09-16 21:53:22 +0530

    added delete todo

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
 mongoose.connect('mongodb+srv://Rajat:rajat@cluster0.gby4v.mongodb.net/TodoApp?retryWrites=true&w=majority');
 
 module.exports = {mongoose};
